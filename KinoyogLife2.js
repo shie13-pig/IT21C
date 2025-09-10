71793a4af0e6890ff9cdbe9c49b067c04a912ce9
Shiela Pearl M. Kinoyog <shiela07132020@gmail.com>
2025-09-09 21:03:58 -1100

    added operators for daily routine

--- KinoyogLife2.js
+++ KinoyogLife2.js
@@ -5,3 +5,11 @@ const yearLevel = "2nd year";
 console.log("Name:", Name);
 console.log("Course:", course);
 console.log("Year Level:", yearLevel);
+
+const hoursSleep = 7;
+const hoursStudy = 4;
+const totalDailyHours = hoursSleep + hoursStudy;
+
+console.log("Hours of Sleep:", hoursSleep);
+console.log("Hours of Study:", hoursStudy);
+console.log("Total Daily Routine Hours:", totalDailyHours)
