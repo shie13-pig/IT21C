0a9e939f64600bddc229d404a6736986dd748a77
Shiela Pearl M. Kinoyog <shiela07132020@gmail.com>
2025-09-09 21:01:39 -1100

    added student variables

---
+++ KinoyogLife2.js
@@ -0,0 +1,7 @@
+const Name = "Shiela Pearl M. Kinoyog";
+const course = "Bachelor of Science in Information Technology";
+const yearLevel = "2nd year";
+
+console.log("Name:", Name);
+console.log("Course:", course);
+console.log("Year Level:", yearLevel);
